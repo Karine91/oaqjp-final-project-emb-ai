0314840d506f6b513acd91aff4bc5d0d3358a17f
Karine91 <26932594+Karine91@users.noreply.github.com>
2026-04-30 10:52:04 +0200

    fix pylint err

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Module providing a server routes for detecting emotions."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,11 +8,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """Function gets detected emotion."""
     text_input = request.args.get("textToAnalyze")
     res = emotion_detector(text_input)
     if res["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
-    emotions = {k: v for k, v in res.items() if k is not "dominant_emotion"}
+    emotions = {k: v for k, v in res.items() if k != "dominant_emotion"}
     emotions_str = ", ".join([f'"{k}": {v}' for k, v in emotions.items()])
     return (
         f"For the given statement, the system response is {emotions_str}.",
@@ -20,6 +23,7 @@ def detect_emotion():
 
 @app.route("/")
 def render_index():
+    """Render home page"""
     return render_template("index.html")
 
 
